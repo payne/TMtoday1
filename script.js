eb7ba981ac38b9f785b139775f3edd6200ebf020
Matt Payne <Payne@MattPayne.org>
2023-03-23 00:55:30 +0000

    hide a div, show a div

--- script.js
+++ script.js
@@ -3,4 +3,8 @@ function showEvaluators() {
   console.log('click the link');
   const all = document.getElementById('all');
   const speakers = document.getElementById('speakers');
+  console.log(all);
+  console.log(speakers);
+  speakers.hidden = false;
+  all.hidden = true;
 }
